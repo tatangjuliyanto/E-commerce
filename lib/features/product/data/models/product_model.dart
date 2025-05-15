c81dcd24d5f969c8682a3be8d1284c8a10d57490
tatangjuliyanto <tatangjulianto05@gmail.com>
2025-05-16 01:42:48 +0700

    create and fixit

--- lib/features/product/data/models/product_model.dart
+++ lib/features/product/data/models/product_model.dart
@@ -5,16 +5,14 @@ class ProductModel extends Product {
     required super.id,
     required super.name,
     required super.price,
-    required super.description,
     required super.imageUrl,
   });
   factory ProductModel.fromJson(Map<String, dynamic> json) {
     return ProductModel(
-      id: json['id'] as String,
-      name: json['name'] as String,
+      id: json['id'].toString(),
+      name: json['title'],
       price: (json['price'] as num).toDouble(),
-      description: json['description'] as String,
-      imageUrl: json['imageUrl'] as String,
+      imageUrl: json['image'],
     );
   }
 }
---
+++ lib/features/product/data/resources/product_remote_datasource.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:winky_shop/features/product/data/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+abstract class ProductRemoteDataSource {
+  Future<List<ProductModel>> fetchProducts();
+}
+
+class ProductRemoteDataSourceImpl implements ProductRemoteDataSource {
+  final http.Client client;
+
+  ProductRemoteDataSourceImpl(this.client);
+
+  @override
+  Future<List<ProductModel>> fetchProducts() async {
+    final response = await client.get(
+      Uri.parse('https://fakestoreapi.com/products'),
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = json.decode(response.body);
+      return jsonList.map((json) => ProductModel.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load Products');
+    }
+  }
+}
