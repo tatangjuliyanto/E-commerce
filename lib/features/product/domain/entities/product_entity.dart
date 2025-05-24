782f6058a4b9ae938f5409105e17d35884d7342c
tatangjuliyanto <tatangjulianto05@gmail.com>
2025-05-24 10:28:51 +0700

    fix the code

--- lib/features/product/domain/entities/product_entity.dart
+++ lib/features/product/domain/entities/product_entity.dart
@@ -11,21 +11,3 @@
 //     required this.imageUrl,
 //   });
 // }
-
-// class ProductModel extends Product {
-//   ProductModel({
-//     required super.id,
-//     required super.name,
-//     required super.price,
-//     required super.imageUrl,
-//   });
-
-//   factory ProductModel.fromJson(Map<String, dynamic> json) {
-//     return ProductModel(
-//       id: json['id'].toString(),
-//       name: json['title'],
-//       price: (json['price'] as num).toDouble(),
-//       imageUrl: json['image'],
-//     );
-//   }
-// }
